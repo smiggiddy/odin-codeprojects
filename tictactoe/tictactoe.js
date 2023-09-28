c8ce75acd1cd0527b26fecdf6a877cc9d6f8d274
Smig Tech <smig@smig.tech>
2023-09-27 20:00:38 -0400

    feat: tictactoe finished

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -17,15 +17,19 @@ else if its a tie , it should prompt the user to pick a new answer
 until a winner is picked
 */
 
-let rpc = ["rock", "paper", "scissors"]
+// index 0 = wins, index 1 = loss
+// index 2 = ties
+let CPUScore = [0,0,0];
+let userScore = [0,0,0];
 
-let usersChoice = () => {
-    let userChoice = prompt("Rock, Paper, or Scissors? ");
-    return userChoice.toLocaleLowerCase();
+let playRound = (choice) => {
+    let uc = choice.target.dataset.item; 
+    cpc = getCPUChoice();
+    getResults(uc, cpc);
 }
 
 let getCPUChoice = () => {
-    // Generate a random number between 0 and 2
+    let rpc = ["rock", "paper", "scissors"];
     let choice = Math.floor(Math.random() * 3);
 
     return rpc[choice];
@@ -33,17 +37,53 @@ let getCPUChoice = () => {
 
 function getResults(playerSelection, cpuSelection) {
     if (playerSelection === cpuSelection) {
+        updateScore('tie')
         return "tie";
     } else if (playerSelection == "rock" && cpuSelection == "scissors" ||
                 playerSelection == "paper" && cpuSelection == "rock" || 
                 playerSelection == "scissors" && cpuSelection == "paper") {
+        updateScore('user');
         return `You Win! ${playerSelection} beats ${cpuSelection}`;
     } else {
+        updateScore('cpu');
         return `You Lose! ${cpuSelection} beats ${playerSelection}`;
     }
 }
 
-uc = usersChoice();
-cpc = getCPUChoice();
-console.log(uc, cpc);
-console.log(getResults(uc, cpc));
+function updateScore(winner) {
+   if (winner === 'user') {
+        userScore[0] += 1;
+        CPUScore[1] += 1;
+    } else if (winner === 'cpu') {
+        CPUScore[0] += 1;
+        userScore[1] += 1;
+    } else {
+        CPUScore[2] += 1;
+        userScore[2] += 1;
+    } 
+
+    // Need a better way to target multiple elements, but this works for now
+    let userScoreDiv = document.querySelector('.user-row');
+    let cpuScoreDiv = document.querySelector('.cpu-row');
+    userScoreDiv.querySelector('.user-win').textContent = userScore[0];
+    userScoreDiv.querySelector('.user-loss').textContent = userScore[1];
+    userScoreDiv.lastElementChild.textContent = userScore[2];
+    cpuScoreDiv.querySelector('.cpu-win').textContent = CPUScore[0];
+    cpuScoreDiv.querySelector('.cpu-loss').textContent = CPUScore[1];
+    cpuScoreDiv.lastElementChild.textContent = CPUScore[2];
+
+    if (userScore[0] === 5 || CPUScore === 5) {
+        let showWinner = document.querySelector('.score .title-text');
+        if(userScore[0] === 5){
+        showWinner.textContent = `YOU WON!`;
+        } else if(CPUScore[0] === 5) {
+        showWinner.textContent = `YOU LOST!`;
+        }
+        console.log('Game Over');
+    }
+    // userScoreDiv.textContent = userScore; //`Your Score: ${userScore} | CPU Score: ${CPUScore}`;
+    // console.log(userScore, CPUScore);
+}
+
+let choice = document.querySelectorAll('button');
+choice.forEach( (c) => c.addEventListener('click', playRound));
