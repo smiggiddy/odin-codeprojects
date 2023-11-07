bcf0be5888c964a0958f2586c961af97d78960d7
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2023-11-07 10:33:06 -0500

    feat: added javascript error validation

--- signup_form/js/script.js
+++ signup_form/js/script.js
@@ -1,14 +1,31 @@
 const name = document.querySelector('#name'),
       email = document.querySelector('#email'),
       password = document.querySelector('#password'),
-      passwordConfirmation = document.querySelector('#password-confirmation')
+      passwordConfirmation = document.querySelector('#password-confirmation'),
+      formInputs = document.querySelectorAll('.form-input')
 
+password.addEventListener('keyup', e => {
+    let error = '*Password must be at least 8 characters!';
+    let errorDiv = password.nextElementSibling;
 
-
-passwordConfirmation.addEventListener('keyup', e => {
-    if (password.value === e.target.value ) {
-        console.log('they equal');
+    if (password.value.length < 8) {
+        password.style.borderColor = "#EF6262";
+        errorDiv.textContent = error;
     } else {
-        console.log('They don\'t equal');
+        password.style.borderColor = "";
+        errorDiv.textContent = "";
     }
 })
+
+passwordConfirmation.addEventListener('keyup', e => {
+    let error = '*Passwords do not match!';
+    let errorDiv = passwordConfirmation.nextElementSibling;
+
+    if (password.value !== e.target.value ) {
+        passwordConfirmation.style.borderColor = "#EF6262";
+        errorDiv.textContent = error;
+    } else {
+        passwordConfirmation.style.borderColor = "";
+        errorDiv.textContent = "";
+    }
+});
