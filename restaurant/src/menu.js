051c7e2ad5feb8c87533f97804c3fd1a24b15c6b
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2023-12-18 17:54:59 -0500

    feat: module added

---
+++ restaurant/src/menu.js
@@ -0,0 +1,9 @@
+function menuBar() {
+    const items = [
+        "home",
+        "menu",
+        "contact"
+    ];
+
+    
+}
--- restaurant/webpack.config.js
+++ restaurant/webpack.config.js
@@ -1,2 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant - Dev',
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+};
  
