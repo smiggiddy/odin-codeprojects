36bf0c07d66081f80cdbae78e21b60ea57f37362
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2024-02-19 16:01:49 -0500

    feat: updated testing configs

--- testing/.eslintrc.js
+++ testing/.eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["eslint:recommended", "prettier"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    overrides: [
+        {
+            files: ['tests/**/*'],
+            plugins: ['jest'],
+            env: {
+                'jest/globals': true,
+            },
+        },
+    ],
 };
