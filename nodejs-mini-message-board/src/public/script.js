289b95d957544e8ea839e64f27b6f1f81bfe9370
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2025-01-01 16:40:49 -0500

    feat: add footer and styling

--- nodejs-mini-message-board/src/public/script.js
+++ nodejs-mini-message-board/src/public/script.js
@@ -2,3 +2,5 @@ function test(event) {
   const messageId = event.target.dataset.messageId;
   window.location.href = `comment?id=${messageId}`;
 }
+
+document.getElementById("year").textContent = `${new Date().getFullYear()}`;
