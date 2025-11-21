6deca23974c2da53aa5f4058324a4274a375e006
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2025-11-21 07:27:59 -0500

    feat: minor styling fixes

--- blog/frontend/src/components/Comments.jsx
+++ blog/frontend/src/components/Comments.jsx
@@ -1,5 +1,35 @@
 import { useLoaderData } from "react-router-dom";
 import AddComment from "./AddComment";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0.5em 0;
+`;
+
+const Flex = styled(Container)`
+  display: flex;
+`;
+
+const Col = styled(Flex)`
+  flex-direction: column;
+`;
+
+const CommentAuthor = styled.p`
+  font-weight: 700;
+`;
+
+const CommentDate = styled.p`
+  font-weight: 200;
+`;
+
+function CommentMetaData(props) {
+  return (
+    <Col>
+      <CommentAuthor>{props.name}</CommentAuthor>
+      <CommentDate>{props.date.substring(0, 10)}</CommentDate>
+    </Col>
+  );
+}
 
 export default function Comments() {
   const { comments } = useLoaderData();
@@ -9,11 +39,10 @@ export default function Comments() {
       {comments.length > 0 ? (
         comments.map((c) => {
           return (
-            <div key={c.id}>
-              <p>
-                "{c.content}" <span>-{c.name}</span>
-              </p>
-            </div>
+            <Container key={c.id}>
+              <CommentMetaData name={c.name} date={c.dateCreated} />
+              <p>{c.content}</p>
+            </Container>
           );
         })
       ) : (
